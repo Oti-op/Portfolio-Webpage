880d977712757659444ca88175083acd65b25bc6
user <otiteos49@gmail.com>
2025-12-01 23:05:57 +0000

    chatbot feature

--- navication/chatbot.py
+++ navication/chatbot.py
@@ -1,3 +1,21 @@
 import streamlit as st
 
 st.title("Chatbot")
+
+if "messages" not in st.session_state:
+    st.session_state.message = []
+
+for message in st.session_stage.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+if prompt := st.chat_input("whats up?"):
+    with st.chat_message("user"):
+        st.markdown(prompt)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = f"Echo: {prompt}"
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    st.session_state.message.append({"role": "assistant", "content": response})
